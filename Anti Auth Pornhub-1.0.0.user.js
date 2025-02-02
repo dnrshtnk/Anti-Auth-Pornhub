68bf3c57e327da854a0d6ce56aad90ba324789c1
Munez <128070378+dnrshtnk@users.noreply.github.com>
2025-02-02 20:05:11 +0300

    Add files via upload

---
+++ Anti Auth Pornhub-1.0.0.user.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         Anti Auth Pornhub
+// @namespace    https://github.com
+// @homepage     https://github.com
+// @version      1.0.0
+// @description  Anti Auth Pornhub
+// @icon         https://di.phncdn.com/www-static/favicon.ico
+// @author       Munez D
+// @match        *://*.pornhub.com/*
+// @downloadURL  https://github.com
+// @updateURL    https://github.com
+// @grant        GM_getResourceText
+// @grant        GM_addStyle
+// @connect      pornhub.com
+// @grant        GM_getResourceURL
+// @run-at       document-body
+// ==/UserScript==
+
+GM_addStyle('.isOpenMTubes { overflow: visible !important; }');
+GM_addStyle('.topSectionGrid{display: block !important; }');
+
+window.onload = function() {
+const ageVerificationContainerElement = document.getElementById("modalWrapMTubes");
+const ageVerificationWrapperElement1 = document.getElementById("ageDisclaimerOverlay");
+const ageVerificationWrapperElement2 = document.getElementById("ageDisclaimerMainBG");
+const ageVerificationWrapperElement3 = document.getElementById("cookieBannerWrapper");
+const ageVerificationWrapperElement4 = document.getElementById("cookieBanner");
+const ageVerificationWrapperElement5 = document.querySelector('.sideColumn');
+ageVerificationContainerElement.remove();
+ageVerificationWrapperElement1.remove();
+ageVerificationWrapperElement2.remove();
+ageVerificationWrapperElement3.remove();
+ageVerificationWrapperElement4.remove();
+ageVerificationWrapperElement5.remove();
+}
+
