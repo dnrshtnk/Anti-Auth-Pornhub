1c76ad3c0d2136f837c8ac7d980ddae3e0c74f1b
Munez <128070378+dnrshtnk@users.noreply.github.com>
2025-02-09 12:25:58 +0300

    Update Anti Auth Pornhub-1.0.0.user.js

--- Anti Auth Pornhub-1.0.0.user.js
+++ Anti Auth Pornhub-1.0.0.user.js
@@ -20,17 +20,21 @@ GM_addStyle('.isOpenMTubes { overflow: visible !important; }');
 GM_addStyle('.topSectionGrid{display: block !important; }');
 
 window.onload = function() {
-const ageVerificationContainerElement = document.getElementById("modalWrapMTubes");
-const ageVerificationWrapperElement1 = document.getElementById("ageDisclaimerOverlay");
-const ageVerificationWrapperElement2 = document.getElementById("ageDisclaimerMainBG");
-const ageVerificationWrapperElement3 = document.getElementById("cookieBannerWrapper");
-const ageVerificationWrapperElement4 = document.getElementById("cookieBanner");
-const ageVerificationWrapperElement5 = document.querySelector('.sideColumn');
-ageVerificationContainerElement.remove();
-ageVerificationWrapperElement1.remove();
-ageVerificationWrapperElement2.remove();
-ageVerificationWrapperElement3.remove();
-ageVerificationWrapperElement4.remove();
-ageVerificationWrapperElement5.remove();
-}
+    const ageVerificationWrapperElement6 = document.getElementById("age-verification-wrapper");
+    const ageVerificationWrapperElement7 = document.getElementById("age-verification-container");
+    const ageVerificationContainerElement = document.getElementById("modalWrapMTubes");
+    const ageVerificationWrapperElement1 = document.getElementById("ageDisclaimerOverlay");
+    const ageVerificationWrapperElement2 = document.getElementById("ageDisclaimerMainBG");
+    const ageVerificationWrapperElement3 = document.getElementById("cookieBannerWrapper");
+    const ageVerificationWrapperElement4 = document.getElementById("cookieBanner");
+    const ageVerificationWrapperElement5 = document.querySelector('.sideColumn');
 
+    ageVerificationContainerElement.remove();
+    ageVerificationWrapperElement1.remove();
+    ageVerificationWrapperElement2.remove();
+    ageVerificationWrapperElement3.remove();
+    ageVerificationWrapperElement4.remove();
+    ageVerificationWrapperElement5.remove();
+    ageVerificationWrapperElement6.remove();
+    ageVerificationWrapperElement7.remove();
+}
