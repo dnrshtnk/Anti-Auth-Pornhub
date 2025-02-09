3a8f6a404a5535b85f1305bf81aa0d0655343411
Munez <128070378+dnrshtnk@users.noreply.github.com>
2025-02-09 12:53:51 +0300

    Update Anti Auth Pornhub-1.0.0.user.js

--- Anti Auth Pornhub-1.0.0.user.js
+++ Anti Auth Pornhub-1.0.0.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @name         Anti Auth Pornhub
+// @name         Anti Auth Pornhub New
 // @namespace    https://github.com
 // @homepage     https://github.com
-// @version      1.0.1
-// @description  Anti Auth Pornhub
+// @version      1.0.2
+// @description  Anti Auth Pornhub New
 // @icon         https://di.phncdn.com/www-static/favicon.ico
 // @author       Munez D
 // @match        *://*.pornhub.com/*
-// @downloadURL  https://github.com
-// @updateURL    https://github.com
+// @downloadURL  https://github.com/dnrshtnk/Anti-Auth-Pornhub/blob/main/Anti%20Auth%20Pornhub-1.0.0.user.js
+// @updateURL    https://github.com/dnrshtnk/Anti-Auth-Pornhub/blob/main/Anti%20Auth%20Pornhub-1.0.0.user.js
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // @connect      pornhub.com
@@ -20,22 +20,18 @@ GM_addStyle('.isOpenMTubes { overflow: visible !important; }');
 GM_addStyle('.topSectionGrid{display: block !important; }');
 
 window.onload = function() {
-    const ageVerificationContainerElement = document.getElementById("age-verification-container");
-    ageVerificationContainerElement.remove();
-    const ageVerificationWrapperElement = document.getElementById("age-verification-wrapper");
-    ageVerificationWrapperElement.remove();
-
     const modalWrapMTubes = document.getElementById("modalWrapMTubes");
     const ageVerificationWrapperElement1 = document.getElementById("ageDisclaimerOverlay");
     const ageVerificationWrapperElement2 = document.getElementById("ageDisclaimerMainBG");
     const ageVerificationWrapperElement3 = document.getElementById("cookieBannerWrapper");
     const ageVerificationWrapperElement4 = document.getElementById("cookieBanner");
     const ageVerificationWrapperElement5 = document.querySelector('.sideColumn');
+    const guidedTour_watchPagePC = document.getElementById("guidedTour_watchPagePC");
     modalWrapMTubes.remove();
     ageVerificationWrapperElement1.remove();
     ageVerificationWrapperElement2.remove();
     ageVerificationWrapperElement3.remove();
     ageVerificationWrapperElement4.remove();
     ageVerificationWrapperElement5.remove();
-
+    guidedTour_watchPagePC.remove();
 }
