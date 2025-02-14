8b6e4b9f6cda4f605a9c38a968032ba374357393
Munez <128070378+dnrshtnk@users.noreply.github.com>
2025-02-14 13:03:15 +0300

    Update Anti_Auth_Pornhub.js

--- Anti_Auth_Pornhub.js
+++ Anti_Auth_Pornhub.js
@@ -21,27 +21,27 @@
 
 GM_addStyle('.isOpenMTubes { overflow: visible !important; }');
 GM_addStyle('.topSectionGrid{display: block !important; }');
-
+GM_addStyle('#ageDisclaimerOverlay,#ageVerificationOverlay{backdrop-filter: blur(0px) !important; -webkit-backdrop-filter: blur(0px) !important; };');
+GM_addStyle('.is-blurred #mobileContainer #videoContainer [class^=mhp1138_videoPoster]{filter: blur(0px) !important;}');
+GM_addStyle('.is-blurred #mobileContainer .main_content img,.is-blurred #mobileContainer [class^=mhp1138_thumb]{filter: blur(0px) !important;}');
+GM_addStyle('.is-blurred #mobileContainer iframe{filter: blur(0px) !important;}');
+GM_addStyle('.is-blurred #mobileContainer img{filter: blur(0px) !important;}');
+GM_addStyle('.is-blurred #mobileContainer video{filter: blur(0px) !important;}');
+GM_addStyle('body.blurredAVModal{filter: blur(0px) !important; opacity: 1 !important;}');
 window.onload = function() {
-    const ageVerificationOverlay = document.getElementById("ageVerificationOverlay");
-    const ageverificationwrapper = document.getElementById("age-verification-wrapper");
-    const ageverificationcontainer = document.getElementById("age-verification-container");
     const modalWrapMTubes = document.getElementById("modalWrapMTubes");
-    const ageVerificationWrapperElement1 = document.getElementById("ageDisclaimerOverlay");
-    const ageVerificationWrapperElement2 = document.getElementById("ageDisclaimerMainBG");
-    const ageVerificationWrapperElement3 = document.getElementById("cookieBannerWrapper");
-    const ageVerificationWrapperElement4 = document.getElementById("cookieBanner");
-    const ageVerificationWrapperElement5 = document.querySelector('.sideColumn');
-    const guidedTour_watchPagePC = document.getElementById("guidedTour_watchPagePC");
-    ageVerificationOverlay.remove();
-    ageverificationwrapper.remove();
-    ageverificationcontainer.remove();
     modalWrapMTubes.remove();
+    const ageVerificationWrapperElement1 = document.getElementById("ageDisclaimerOverlay");
     ageVerificationWrapperElement1.remove();
+    const ageVerificationWrapperElement2 = document.getElementById("ageDisclaimerMainBG");
     ageVerificationWrapperElement2.remove();
+    const ageVerificationWrapperElement3 = document.getElementById("cookieBannerWrapper");
     ageVerificationWrapperElement3.remove();
+    const ageVerificationWrapperElement4 = document.getElementById("cookieBanner");
     ageVerificationWrapperElement4.remove();
+    const ageVerificationWrapperElement5 = document.querySelector('.sideColumn');
     ageVerificationWrapperElement5.remove();
+    const guidedTour_watchPagePC = document.getElementById("guidedTour_watchPagePC");
     guidedTour_watchPagePC.remove();
 }
 })();
